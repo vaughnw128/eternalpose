88148025884f45540cbfd554df5574f1267e69b3
vaughnwoerpel@gmail.com <vaughnwoerpel@gmail.com>
2024-06-15 21:16:20 -0400

    Update

--- src/eternalpose/main.go
+++ src/eternalpose/main.go
@@ -11,13 +11,19 @@ func cleanString(s string) string {
 	return strings.Join(strings.Fields(strings.Trim(s, " ")), " ")
 }
 
+func getCover(l string) string {
+	return "lol"
+}
+
 func main() {
 	c := colly.NewCollector()
 
+	// Additional manga can be added here to alert on
+	mangaSite := "https://tcbscans.com"
 	mangaRegex := [2]string{"One Piece Chapter \\d{4}$", "Jujutsu Kaisen Chapter \\d{3}$"}
 
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		// link := e.Attr("href")
+		link := e.Attr("href")
 		if e.Text == "" {
 			return
 		}
@@ -25,7 +31,7 @@ func main() {
 		for _, regexTerm := range mangaRegex {
 			match, _ := regexp.MatchString(regexTerm, mangaTitle)
 			if match {
-				fmt.Println(mangaTitle)
+				fmt.Printf("%s - %s\n", mangaTitle, mangaSite+link)
 			}
 		}
 	})
@@ -35,7 +41,7 @@ func main() {
 		fmt.Println("Visiting", r.URL.String())
 	})
 
-	err := c.Visit("https://tcbscans.com/")
+	err := c.Visit(mangaSite)
 	if err != nil {
 		fmt.Println("Error visiting TCBScans.")
 	}
